84212aeefd1f0fc0e2279d743ddf9979295fc896
Jean A. Eckelberg <fyrelynx11@gmail.com>
2025-07-15 00:04:13 -0500

    feat: Use OTEL for pushing logs

--- main.go
+++ main.go
@@ -1,16 +1,92 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
-	"log"
+	"github.com/joho/godotenv"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/metric"
+	"go.opentelemetry.io/otel/trace"
+	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
+func init() {
+	_ = godotenv.Load()
+}
+
 func main() {
+	ctx := context.Background()
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	otelShutdown, err := setupOTel(ctx)
+	if err != nil {
+		slog.Warn("Otel Setup Failed",
+			"error", err,
+		)
+	}
+
+	defer func() {
+		err = errors.Join(err, otelShutdown(ctx))
+
+		slog.Info("Application Closing", "error", err)
+	}()
+
+	//TESTING CODE
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		slog.Info("Handling request",
+			"path", r.URL.Path,
+			"method", r.Method,
+			"remote_addr", r.RemoteAddr,
+		)
 		fmt.Fprintf(w, "Hello World")
 	})
 
-	log.Println("Listening on port 8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	slog.Info("Starting server", "port", 8080)
+	if err = http.ListenAndServe(":8080", nil); err != nil {
+		slog.Error("Server failed", "error", err)
+		os.Exit(1)
+	}
+
+	name := "orchestration-service"
+	tracer := otel.Tracer(name)
+	meter := otel.Meter(name)
+
+	// Attributes represent additional key-value descriptors that can be bound
+	// to a metric observer or recorder.
+	commonAttrs := []attribute.KeyValue{
+		attribute.String("attrA", "chocolate"),
+		attribute.String("attrB", "raspberry"),
+		attribute.String("attrC", "vanilla"),
+	}
+
+	runCount, err := meter.Int64Counter("run", metric.WithDescription("The number of times the iteration ran"))
+	if err != nil {
+		slog.Error("Error Encountered",
+			"error", err)
+	}
+
+	// Work begins
+	ctx, span := tracer.Start(
+		ctx,
+		"CollectorExporter-Example",
+		trace.WithAttributes(commonAttrs...))
+	defer span.End()
+	for i := 0; i < 10; i++ {
+		_, iSpan := tracer.Start(ctx, fmt.Sprintf("Sample-%d", i))
+		runCount.Add(ctx, 1, metric.WithAttributes(commonAttrs...))
+		slog.Info(fmt.Sprintf("Doing really hard work (%d / 10)\n", i+1), "count", i+1)
+
+		<-time.After(time.Second)
+		iSpan.End()
+	}
+
+	slog.Info("Done!")
 }
---
+++ otel.go
@@ -0,0 +1,182 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"go.opentelemetry.io/contrib/bridges/otelslog"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlplog/otlploggrpc"
+	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/log/global"
+	"go.opentelemetry.io/otel/propagation"
+	sdklog "go.opentelemetry.io/otel/sdk/log"
+	sdkmetric "go.opentelemetry.io/otel/sdk/metric"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.26.0"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"log/slog"
+	"os"
+)
+
+func setupOTel(
+	ctx context.Context,
+) (shutdown func(context.Context) error, err error) {
+	var shutdownFuncs []func(context.Context) error
+
+	shutdown = func(ctx context.Context) error {
+		var err error
+
+		for _, fn := range shutdownFuncs {
+			err = errors.Join(err, fn(ctx))
+		}
+
+		shutdownFuncs = nil
+
+		return err
+	}
+
+	handleErr := func(inErr error) {
+		err = errors.Join(inErr, shutdown(ctx))
+	}
+
+	res, err := newResource()
+	if err != nil {
+		handleErr(err)
+		return
+	}
+
+	conn, err := initConn()
+	if err != nil {
+		handleErr(err)
+		return
+	}
+
+	loggerShutdown, err := initLoggerProvider(ctx, res, conn)
+	if err != nil {
+		handleErr(err)
+		return
+	}
+
+	shutdownFuncs = append(shutdownFuncs, loggerShutdown)
+
+	meterShutdown, err := initMeterProvider(ctx, res, conn)
+	if err != nil {
+		handleErr(err)
+		return
+	}
+
+	shutdownFuncs = append(shutdownFuncs, meterShutdown)
+
+	tracerShutdown, err := initTracerProvider(ctx, res, conn)
+	if err != nil {
+		handleErr(err)
+		return
+	}
+
+	shutdownFuncs = append(shutdownFuncs, tracerShutdown)
+
+	return
+}
+
+func newResource() (*resource.Resource, error) {
+	return resource.Merge(resource.Default(),
+		resource.NewWithAttributes(semconv.SchemaURL,
+			semconv.ServiceName("orchestration-service"), // TODO: Make Environment Variables
+			semconv.ServiceVersion("0.1.0"),
+		))
+}
+
+var serviceName = semconv.ServiceNameKey.String("test-service")
+
+// Initialize a gRPC connection to be used by both the tracer, meter, & log
+// providers.
+func initConn() (*grpc.ClientConn, error) {
+	// It connects the OpenTelemetry Collector through local gRPC connection.
+	conn, err := grpc.NewClient("otel-collector:4317",
+		// Note the use of insecure transport here. TLS is recommended in production.
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create gRPC connection to collector: %w", err)
+	}
+
+	return conn, err
+}
+
+// Initializes an OTLP exporter, and configures the corresponding trace provider.
+func initTracerProvider(ctx context.Context, res *resource.Resource, conn *grpc.ClientConn) (func(context.Context) error, error) {
+	// Set up a trace exporter
+	traceExporter, err := otlptracegrpc.New(ctx, otlptracegrpc.WithGRPCConn(conn))
+	if err != nil {
+		return nil, fmt.Errorf("failed to create trace exporter: %w", err)
+	}
+
+	// Register the trace exporter with a TracerProvider, using a batch
+	// span processor to aggregate spans before export.
+	bsp := sdktrace.NewBatchSpanProcessor(traceExporter)
+	tracerProvider := sdktrace.NewTracerProvider(
+		sdktrace.WithSampler(sdktrace.AlwaysSample()),
+		sdktrace.WithResource(res),
+		sdktrace.WithSpanProcessor(bsp),
+	)
+	otel.SetTracerProvider(tracerProvider)
+
+	// Set global propagator to tracecontext (the default is no-op).
+	otel.SetTextMapPropagator(propagation.TraceContext{})
+
+	// Shutdown will flush any remaining spans and shut down the exporter.
+	return tracerProvider.Shutdown, nil
+}
+
+// Initializes an OTLP exporter, and configures the corresponding meter provider.
+func initMeterProvider(ctx context.Context, res *resource.Resource, conn *grpc.ClientConn) (func(context.Context) error, error) {
+	metricExporter, err := otlpmetricgrpc.New(ctx, otlpmetricgrpc.WithGRPCConn(conn))
+	if err != nil {
+		return nil, fmt.Errorf("failed to create metrics exporter: %w", err)
+	}
+
+	meterProvider := sdkmetric.NewMeterProvider(
+		sdkmetric.WithReader(sdkmetric.NewPeriodicReader(metricExporter)),
+		sdkmetric.WithResource(res),
+	)
+	otel.SetMeterProvider(meterProvider)
+
+	return meterProvider.Shutdown, nil
+}
+
+// initLoggerProvider configures and initializes the OpenTelemetry LoggerProvider
+func initLoggerProvider(ctx context.Context, res *resource.Resource, conn *grpc.ClientConn) (func(context.Context) error, error) {
+	consoleHandler := slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+		Level:     slog.LevelInfo,
+		AddSource: true,
+	})
+
+	// Create the OTLP log exporter (e.g., gRPC to localhost:4317)
+	logExporter, err := otlploggrpc.New(ctx, otlploggrpc.WithGRPCConn(conn))
+	if err != nil {
+		return nil, fmt.Errorf("failed to create log exporter: %w", err)
+	}
+
+	// Create the logger provider with the exporter and batch processor
+	loggerProvider := sdklog.NewLoggerProvider(
+		sdklog.WithProcessor(
+			sdklog.NewBatchProcessor(logExporter), // Use a batch processor for efficiency
+		),
+		sdklog.WithResource(res),
+	)
+
+	// Create handlers
+	otelLogHandler := otelslog.NewHandler("orchestration-logs",
+		otelslog.WithLoggerProvider(loggerProvider))
+
+	multiHandler := NewMultiHandler(consoleHandler, otelLogHandler)
+	logger := slog.New(multiHandler)
+	slog.SetDefault(logger)
+	global.SetLoggerProvider(loggerProvider)
+
+	return loggerProvider.Shutdown, nil
+}
